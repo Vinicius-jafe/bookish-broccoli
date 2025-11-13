99e40ae405af6fd586f215448b53fd03b1907c93
Vinicius-jafe <viniciusjafe@gmail.com>
2025-11-13 18:56:40 -0300

    minha segunda mensagem 0

--- frontend/src/components/SiteShell.jsx
+++ frontend/src/components/SiteShell.jsx
@@ -1,5 +1,5 @@
 // Agrupados para fins de separação, mas idealmente estariam em arquivos como SiteShell.jsx e Home.jsx
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../components/ui/button';
 import { Card, CardHeader, CardTitle, CardContent } from '../components/ui/card';
